72b09f0be25f3e7006c14282625a68c52bf50256
Jumbo88888 <physical2004@gmail.com>
2016-12-21 11:22:30 +0800

    samba-4.1.5.tar.gz for ARM - Compile OK

---
+++ EMBED/samba/PATCH/ldb_version.h
@@ -0,0 +1,3 @@
+/* This define and header file is needed so we can include ldb.h
+ *  * without building ldb properly */
+#define LDB_VERSION "dummy ldb version for ldb.h without ldb"
--- EMBED/samba/auto.sh
+++ EMBED/samba/auto.sh
@@ -4,6 +4,7 @@ PACKAGE=$CURDIR/PACKAGE
 PATCH=$CURDIR/PATCH
 TARGET=$CURDIR/TARGET
 OBJ=$CURDIR/OBJ
+SAMBA=$TARGET/samba-4.1.5
 
 if [ ! -d "$PACKAGE" ]; then
 	mkdir $PACKAGE
@@ -22,11 +23,11 @@ if [ ! -d "$OBJ" ]; then
 fi
 
 echo "************ samba-4.1.5.tar.gz ***********************"
-if [ ! -d $TARGET/samba-4.1.5 ]; then
+if [ ! -d $SAMBA ]; then
     cd $PACKAGE
     tar xvf "samba-4.1.5.tar.gz" -C $TARGET
 
-    cd $TARGET/samba-4.1.5/
+    cd $SAMBA/
     cp $PATCH/arm.txt .
 
     # Patch file for Embedded system
@@ -47,15 +48,19 @@ if [ ! -d $TARGET/samba-4.1.5 ]; then
     cp $PATCH/asn1_compile ./bin/default/source4/heimdal_build
     cp $PATCH/compile_et   ./bin/default/source4/heimdal_build
 
-    export PATH=/home/freeman/test/1/Python/X86/python/OBJ/bin:$TARGET/samba-4.1.5/bin/default/source4/heimdal_build/:$PATH
-    export LD_LIBRARY_PATH=/home/freeman/test/1/Python/X86/python/OBJ/lib:$LD_LIBRARY_PATH
+    # Create a ldb_version.h and patch it
+    mkdir $SAMBA/source3/include/autoconf
+    cp $PATCH/ldb_version.h   ./source3/include/autoconf/
+
+    export PATH=/home/freeman/test/1/Python/X86/python/OBJ/bin:$SAMBA/bin/default/source4/heimdal_build/:$PATH
+    export LD_LIBRARY_PATH=/home/freeman/test/1/Python/X86/python/OBJ/lib:/home/freeman/test/1/Python/EMBED/python/OBJ/lib/:$LD_LIBRARY_PATH
     CC=arm-linux-gnueabihf-gcc-4.8.5 \
         AR=arm-linux-gnueabihf-ar \
         CPP=arm-linux-gnueabihf-cpp \
         RANLIB=arm-linux-gnueabihf-gcc-ranlib  \
         PYTHON=/home/freeman/test/1/Python/X86/python/OBJ/bin/python \
-        CFLAGS="-I/home/freeman/test/1/Python/X86/python/OBJ/include/python2.7" \
-        LIBDIR="/home/freeman/test/1/Python/EMBED/python/OBJ/lib/" \
+        CFLAGS="-I/home/freeman/test/1/Python/EMBED/python/OBJ/include/python2.7 -I$SAMBA/lib/talloc/ -I$SAMBA/lib/tevent -I$SAMBA/lib/tdb/include -I$SAMBA/lib/ldb/include/ -I$SAMBA/lib/ldb/ -I$SAMBA/source3/include/autoconf/" \
+        LIBDIR="-L/home/freeman/test/1/Python/EMBED/python/OBJ/lib/" \
         PYTHON_CONFIG="/home/freeman/test/1/Python/X86/python/OBJ/bin/python-config" \
         python_LDFLAGS="-L/home/freeman/test/1/Python/EMBED/python/OBJ/lib/ -lpython2.7" \
         python_LIBDIR="/home/freeman/test/1/Python/EMBED/python/OBJ/lib/" \
@@ -74,6 +79,11 @@ if [ ! -d $TARGET/samba-4.1.5 ]; then
         --without-cluster-support \
         --enable-debug \
         --enable-selftest \
+        --disable-gnutls \
+        --builtin-libraries=talloc,pytalloc-util \
+        --bundled-libraries=talloc,pytalloc-util,tdb,tevent,ldb \
+
     make
     make install
 fi
+
